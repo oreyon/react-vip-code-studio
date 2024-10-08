bc3a4ce2747070e1a849e2584767be647aa88010
oreyon <dontrichpeopledifficulut@gmail.com>
2024-10-08 14:56:27 +0800

    completed 07-conditional-rendering

--- 07-conditional-rendering/src/pages/login.tsx
+++ 07-conditional-rendering/src/pages/login.tsx
@@ -1,18 +1,11 @@
-import { Link } from 'react-router-dom';
 import FormLogin from '../components/Fragments/FormLogin';
 import AuthLayouts from '../components/Layouts/AuthLayouts';
 
 const LoginPage = () => {
 	return (
 		<div className='flex justify-center items-center min-h-screen gap-x-3'>
-			<AuthLayouts titleprops='Login'>
+			<AuthLayouts titleprops='Login' typeprops='login'>
 				<FormLogin />
-				<p className='text-sm mt-5 text-center'>
-					Don't have an account?{' '}
-					<Link to='/register' className='font-bold text-blue-600'>
-						Register
-					</Link>
-				</p>
 			</AuthLayouts>
 		</div>
 	);
--- 07-conditional-rendering/src/pages/register.tsx
+++ 07-conditional-rendering/src/pages/register.tsx
@@ -1,18 +1,11 @@
-import { Link } from 'react-router-dom';
 import FormRegister from '../components/Fragments/FormRegister';
 import AuthLayouts from '../components/Layouts/AuthLayouts';
 
 const RegisterPage = () => {
 	return (
 		<div className='flex justify-center items-center min-h-screen gap-x-3'>
-			<AuthLayouts titleprops='Register'>
+			<AuthLayouts titleprops='Register' typeprops='register'>
 				<FormRegister />
-				<p className='text-sm mt-5 text-center'>
-					Already have an account?{' '}
-					<Link to='/login' className='font-bold text-blue-600'>
-						Login
-					</Link>
-				</p>
 			</AuthLayouts>
 		</div>
 	);
