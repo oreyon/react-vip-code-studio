47d18e67ce4ea4c3f3c6485e99bb505d5d7d3bb6
oreyon <dontrichpeopledifficulut@gmail.com>
2024-10-22 17:13:42 +0800

    completed 09-rendering-list

--- 09-rendering-list/src/components/Fragments/CardProduct.tsx
+++ 09-rendering-list/src/components/Fragments/CardProduct.tsx
@@ -7,7 +7,7 @@ type CardProductProps = {
 
 const CardProduct = (props:CardProductProps) => {
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
             {props.children}
         </div>
     )
@@ -34,7 +34,7 @@ type CardBodyProps = {
 
 const CardBody = (props:CardBodyProps) => {
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
                 <h5 className="text-xl font-semibold tracking-tight text-white">
                     {props.titleProps}
--- 09-rendering-list/src/pages/products.tsx
+++ 09-rendering-list/src/pages/products.tsx
@@ -1,13 +1,46 @@
 import CardProduct from "../components/Fragments/CardProduct.tsx";
 
+const products = [
+    {
+        id: 1,
+        name: "Wuthering Heights",
+        price: "Rp 1.000.000",
+        image: "/images/book-1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore laudantium magnam minus obcaecati voluptatem? Commodi nesciunt quia vel. Doloribus?"
+    },
+    {
+        id: 2,
+        name: "Jane Eyre",
+        price: "Rp 1.000.000",
+        image: "/images/book-2.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore laudantium magnam minus obcaecati voluptatem? Commodi nesciunt quia vel. Doloribus?"
+    },
+    {
+        id: 3,
+        name: "The Picture of Dorian Gray",
+        price: "Rp 1.000.000",
+        image: "/images/book-3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore laudantium magnam minus obcaecati voluptatem? Commodi nesciunt quia vel. Doloribus?"
+    },
+    {
+        id: 4,
+        name: "The Great Gatsby",
+        price: "Rp 1.000.000",
+        image: "/images/book-4.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore laudantium magnam minus obcaecati voluptatem? Commodi nesciunt quia vel. Doloribus?"
+    }
+]
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-        <CardProduct>
-            <CardProduct.Header hrefProps={"#"} srcProps={'/images/book-1.jpg'} altProps={"products"}/>
-            <CardProduct.Body titleProps={'Book'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore laudantium magnam minus obcaecati voluptatem? Commodi nesciunt quia vel. Doloribus?</CardProduct.Body>
-            <CardProduct.Footer priceProps={'Rp 1.000.000'} />
-        </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header hrefProps={"#"} srcProps={product.image} altProps={product.name}/>
+                    <CardProduct.Body titleProps={product.name}>{product.description}</CardProduct.Body>
+                    <CardProduct.Footer priceProps={product.price} />
+                </CardProduct>
+            ))}
         </div>
     )
 }
